c6c890807a280f81565fb70baf6dfc2e4e5fd633
Pranamya Kulal <spacedust26@gmail.com>
2024-05-30 17:46:36 +0530

    responsive on phone

--- main.js
+++ main.js
@@ -22,3 +22,12 @@ var x = setInterval(function () {
   document.getElementById("seconds").innerHTML = "00";
   }
 }, 1000);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger');
+  const navbar = document.querySelector('.navbar');
+
+  hamburger.addEventListener('click', function() {
+    navbar.classList.toggle('active');
+  });
+});
